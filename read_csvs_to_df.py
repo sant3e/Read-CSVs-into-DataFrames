aedcf60abf6bf1ca6a1341f72abbd4db6b234835
PhD. Diaconescu Tiberiu <89382386+sant3e@users.noreply.github.com>
2022-05-26 23:32:19 +0300

    improving code

--- read_csvs_to_df.py
+++ read_csvs_to_df.py
@@ -1,14 +1,20 @@
 import glob
 import pandas as pd
 
+# specify path to csv files
 path = "C:/Users/tdiacon/Documents/glassdoor-scraper/src/output/"
 
+# csv files in the path
 files = glob.glob(path + "/*.csv")
+
+# define an empty list to sore content
 data_frame = pd.DataFrame()
 content = []
 
+# checking all csv files in the specified path
 for filename in files:
     df = pd.read_csv(filename, index_col=None, encoding="ISO-8859-1")
     content.append(df)
-
+    
+# converting content to dataframe
 data_frame = pd.concat(content)
